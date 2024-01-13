460f7056263609b272f010e0f3138177c51e2285
Tuba Türkoğlu <92tuba92@gmail.com>
2024-01-14 01:48:17 +0300

    hero.jsx fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Vector from "../../assets/vector3.png";
-import Food1 from "../../assetsbiryani2.png";
+import Food1 from "../../assets/biryani2.png";
 import Food2 from "../../assets/biryani3.png";
 import Food3 from "../../assets/biryani5.png"
 
@@ -30,7 +30,7 @@ const bgImage = {
 }
 const Hero = () => {
 
-    // const [imageId, setImageId] = React.useState(Food1)
+    const [imageId, setImageId] = React.useState(Food1);
 
     return (
 
@@ -45,11 +45,28 @@ const Hero = () => {
                     <div className="grid grid-cols-1 sm:grid-cols-2">
                         { /* text content section*/}
                         <div className="flex flex-col
-                        justify-center">
-                            <h1>Welcome to the Foodie Zone</h1>
+                        justify-center gap-4 pt-12 sm:pt-0
+                        text-center sm:text-left order-2
+sm:order-1">
+                            <h1 className="text-5xl
+                            sm:text-6xl lg:text-7xl font-bold
+                            ">Welcome to the Foodie Zone</h1>
+                            <p className="text-sm">Lorem ipsum dolor
+                                sit amet consectetur,
+                                adipisicing elit. Molestiae,
+                                inventore similique. Esse,
+                                ea necessitatibus velit
+                                maxime beatae est error
+                                vitae!</p>
+                            <div>
+                                <button className="bg-gradient-to-r
+                                    from-primary to-secondary text-white
+                                    px-4 py-2 rounded-full hover:scale-105
+                                    duration-200">Order Now</button>
+                            </div>
                         </div>
-                        
-                        {/*ımage section*/}
+
+                        {/*Image section*/}
                         <div></div>
                     </div>
 
